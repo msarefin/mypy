bcf9c1e80494d9b1b31c75d8c5117ad9f5bbfbce
msarefin <mohammed.arefin@gmail.com>
2020-05-03 11:40:31 -0400

    My first python

---
+++ datatype.py
@@ -0,0 +1,28 @@
+# This is a data type
+
+var1 = "Hello "
+var2 = 3
+var3 = 3.14
+
+
+print(var1)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(var2+var3)
+var3 = "Value changed by user"
+print(var3, type(var3))
+var4 = "Arefin"
+print(var3+" "+var4)
+
+var5 = "3"
+var1 = "12"
+print(var4 + var5)
+
+#adding two strings
+
+print(int(var1) + int(var5))
+print(type(var1))
+var6 = 13.3
+varOut = int(var6)+float(var5)
+print(varOut,type(varOut))
---
+++ multi.py
@@ -0,0 +1,20 @@
+#This is a single line comment
+"""This  is a multi line comment"""
+
+print("Defect 123")
+print ("Defect 345")
+print("defect 678")
+
+print("This is a sigle line","This is another line",end =" ")
+
+print("This is the next line")
+
+print("c:/Suns")
+print("c:\\nsuns")
+print("c:\'narry")
+
+print("Welcome \tto \nPython Tutorial")# This is testing an escape sequence
+
+
+# Escape sequence
+
